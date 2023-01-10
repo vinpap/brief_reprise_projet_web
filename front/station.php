653bd953f48669d82a3dcdeaffe1af7c189e5827
Vincent Papelard <papelardvincent@gmail.com>
2023-01-10 16:18:00 +0100

    Ajout de la carte (c'est moche, mise en forme à retoucher plus tard

--- front/station.php
+++ front/station.php
@@ -1,6 +1,7 @@
 
-    <link rel="stylesheet" href="/asset/css/station.css">
-<div class = "container_station">    
+<link rel="stylesheet" href="/asset/css/station.css">
+<div class = "container_station"> 
+    <div id="map"></div>   
     <div class = "info">
         <h3>Info Station</h3>
         <p>Nom station :</p>
@@ -9,3 +10,11 @@
         <p>Info Station :</p>
     </div>
 </div>
+
+<script type="text/javascript">
+    var map = L.map('map').setView([45.761658, 4.835130], 13);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+</script>
