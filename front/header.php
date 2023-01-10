7aff6d3b69d9bb571ba7eab4665cdf801f825d5b
LaurenceAndraud <laurence.andraud96@gmail.com>
2023-01-10 14:32:43 +0100

    header de fait
    on a un beau header :)

---
+++ front/header.php
@@ -0,0 +1,11 @@
+<div class = "container_header">
+    
+    <img class = "logo" src="/asset/images/logo.png" alt="">
+    
+    <div class = "nav">
+        <a class = "en_cours" href = "http://localhost:3000/main.php"><button>Accueil</button></a>
+        <a href = ""><button>Stations</button></a>
+        <a href = ""><button>Avis</button></a>
+        <a href = ""><button>Contact</button></a>
+    </div>
+</div>
