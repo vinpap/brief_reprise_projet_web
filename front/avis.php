e68b90dd6861d1daa1ddc49b065abf556309d6e1
JoaoAndre-RosaTeixeira <91742556+JoaoAndre-RosaTeixeira@users.noreply.github.com>
2023-01-10 20:42:18 +0100

    fix grid avis

--- front/avis.php
+++ front/avis.php
@@ -23,9 +23,11 @@
 
     <div class = "container_texte">
         <div class = "text">
+            <span class="pseudo_text">Pseudo </span>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vulputate felis augue, vitae ultrices odio mollis vitae. Aliquam ut metus eu sapien varius congue. Vivamus pretium massa eget felis ornare, imperdiet rutrum enim scelerisque. Etiam eget sem eu orci rutrum venenatis. Quisque elementum, metus vel tincidunt tempor, velit enim ultrices dolor, eu varius ante ligula non metus. Proin tempor nisi id sem condimentum laoreet eu gravida sem.
         </div>
         <div class = "text">
+            <span class="pseudo_text">Pseudo </span>
         Nullam massa lacus, blandit a enim sed, sagittis lobortis dolor. Sed blandit, ligula et consequat gravida, arcu justo posuere sem, in finibus odio augue ut risus. Vestibulum pharetra justo non ultrices volutpat. Ut tristique congue ultricies. Phasellus lectus libero, fringilla vitae condimentum nec, feugiat hendrerit metus.
         </div>
     </div>
