29e84ad5fe5040678aeb1e5b02a5623d43e82d12
JoaoAndre-RosaTeixeira <91742556+JoaoAndre-RosaTeixeira@users.noreply.github.com>
2023-01-10 21:59:09 +0100

    change accueil

--- front/accueil.php
+++ front/accueil.php
@@ -1,21 +1,35 @@
 <link rel="stylesheet" href="/asset/css/accueil.css">
 
 <div class="container-accueil">
-    <h3>Bienvenue sur le projet web de la promo CosmoDev</h3>
+    <h2>Bienvenue sur le projet web de la promo CosmoDev</h2>
 
-    Les membres du groupe de travail :
+    <h4>Les membres du groupe de travail :</h4>
     <div class="container_card">
         <div>
+            <div>
             <p>Laurence</p>
+            <div><img src="/asset/images/parisienne.jpg" alt=""></div>
+            </div>
         </div>
         <div>
+            <div>
             <p>Kewser</p>
+            <div><img src="/asset/images/ouighour.jpg" alt=""></div>
+
+            </div>
         </div>
         <div>
+            <div>
             <p>Vincent</p>
+            <div ><img src="/asset/images/prof.jpg" alt=""></div>
+
+            </div>
         </div>
         <div>
+            <div>
             <p>Joao</p>
+            <div ><img src="/asset/images/kratos.jpg" alt=""></div>
+            </div>
         </div>
     </div>
 </div>
