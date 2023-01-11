34a5cc030c1a07a3283d33a651481919eeaf42d1
Vincent Papelard <papelardvincent@gmail.com>
2023-01-11 11:03:10 +0100

    Fin du codage de la map + connexion API

--- front/station.php
+++ front/station.php
@@ -1,13 +1,14 @@
 
+
 <link rel="stylesheet" href="/asset/css/station.css">
 <div class = "container_station"> 
     <div id="map"></div>   
     <div class = "info">
         <h3>Info Station</h3>
-        <p>Nom station :</p>
-        <p>État :</p>
-        <p>Vélos dispos :</p>
-        <p>Info Station :</p>
+        <p>Nom station : <span id="name"></span></p>
+        <p>État : <span id="status"></span></p>
+        <p>Vélos dispos : <span id="bikes"></span></p>
+        <p>Info Station : <span id="station_info"></span></p>
     </div>
 </div>
 
@@ -18,29 +19,38 @@
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
 
-    // Récupérer une liste de toutes les stations dispo
-    // Parcourir la liste et afficher un marqueur aux coordonnées de chaque station
     var url = "https://api.jcdecaux.com/vls/v1/stations?contract=Lyon&apiKey=4ea101268c4a14e763d185db28b8964e40e462de"
     fetch(url)
     .then(function(response) {
       return response.json();
-    }).then(function(json_response){
-
-        for (let i = 0; i < json_response.length; i++){
-            latitude = json_response[i]["position"]["latitude"];
-            longitude = json_response[i]["position"]["longitude"];
-            var marker = new L.Marker([45.761658, 4.835130]);
+    }).then(function(json_content){
+        
+        var stations_list = [];
+        var current_id = 0;
+        for (station of json_content)
+        {
+            
+            let current_station = {
+                station_id: current_id,
+                latitude: station.position.lat,
+                longitude: station.position.lng,
+                name: station.name,
+                status: station.status,
+                bikes: station.available_bikes,
+                info: station.address
+            };
+            stations_list.push(current_station);
+            var marker = new L.Marker([current_station.latitude, current_station.longitude]);
+            marker.on('click', function() {
+                document.getElementById("name").innerHTML = current_station.name;
+                document.getElementById("status").innerHTML = current_station.status;
+                document.getElementById("bikes").innerHTML = current_station.bikes;
+                document.getElementById("station_info").innerHTML = current_station.info;
+            });
             marker.addTo(map);
-        }
+
+        } 
 
     })
-
-    /*var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", "https://api.jcdecaux.com/vls/v1/stations?contract=Lyon&apiKey=4ea101268c4a14e763d185db28b8964e40e462de", false );
-    xmlHttp.send( null );*/
-    
-
-
-
 
 </script>
